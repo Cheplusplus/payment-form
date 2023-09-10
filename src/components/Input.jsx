60875988519de506f269359d224cc90c409031a5
ChePlusPlus <chephoto@rocketmail.com>
2023-09-10 08:33:38 +0200

    No

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,65 +6,50 @@ import { Message } from "../App";
 
 /**
  *
- * @param {object} p
- * @param {string} p.inputID
- * @param {string} p.value
- * @param {string} [p.defaultValue]
- * @param {function} p.setValue
- * @param {function} p.setMessages
- * @param {function} p.clearMessages
- * @param {function | null} [p.valueFormatter]
- * @param {number} [p.maxLength]
- * @param {function[]} [p.validationFns]
- * @param {function|null} [p.updateUI]
- *
+ * @param {object} params
+ * @prop {string} inputID
+ * @prop {string} value
+ * @prop {string} defaultValue
+ * @prop {function} setValue
+ * @prop {function[]} validationFns
+ * @prop {function} setMessages
+ * @prop {function} clearMessages
  * @returns
  */
 const Input = ({
   inputID,
   value,
-  defaultValue = "",
+  defaultValue,
   setValue,
+  validationFns,
   setMessages,
   clearMessages,
-  valueFormatter = null,
-  maxLength = 40,
-  validationFns = [],
-  updateUI = null,
 }) => {
   return (
     <input
       type="text"
       className={inputID}
-      value={value}
-      placeholder={defaultValue}
-      maxLength={maxLength}
-      onFocus={(e) => {
-        e.target.classList.add("selected");
-      }}
-      onBlur={(e) => {
-        valueFormatter !== null && e.target.value !== defaultValue
-          ? setValue(valueFormatter(e.target.value))
+      value={value !== "" ? value : defaultValue}
+      onClick={(e) => {
+        //@ts-ignore
+        e.target.value === defaultValue
+          ? //@ts-ignore
+            (e.target.value = "")
           : null;
-        e.target.classList.remove("selected");
       }}
       onChange={(e) => {
-        setValue((v) => e.target.value);
-        updateUI !== null ? updateUI(inputID, e.target.value) : null;
-        if (validationFns.length === 0) {
+        if (validationFns.length !== 0) {
+          setValue(e.target.value);
           return;
         }
         const res = validate(e.target.value, validationFns);
         clearMessages(inputID);
-        if (res === true) {
-          e.target.classList.remove("input-error");
-        } else {
-          res.map((error) => {
-            const newMessage = Message(inputID, error);
-            setMessages((msgs) => [...msgs, newMessage]);
-          });
-          e.target.classList.add("input-error");
-        }
+        res === true
+          ? setValue(e.target.value)
+          : res.map((error) => {
+              const newMessage = Message(inputID, error.toString());
+              setMessages((msgs) => [...msgs, newMessage]);
+            });
       }}
     />
   );
